6cbf7501a94e489a973b2b95ced5c166a0e6bc55
Elena Andrés <elenaandres08@gmail.com>
2019-12-17 21:16:44 +0100

    unødvendig mange git meldinger

--- src/components/NewRecipe.vue
+++ src/components/NewRecipe.vue
@@ -2,9 +2,18 @@
   <div>
     <label for="recipe">Legg til ny oppskrift:</label>
     <br />
-
-    <textarea v-model="newRecipe" placeholder="Skriv inn oppskrift" />
+    
+    <textarea v-model="newRecipe" placeholder="Tittel" />
     <br />
+    <textarea placeholder="Tags (liste av strenger)" />
+    <br />
+    <select>
+      <option>En ingrediens</option>
+      <option>To ingrediens</option>
+      <option>Tre ingrediens</option>
+    </select>
+    <br />
+    <textarea placeholder="ny ingrediens (navn, kategori, mengde)" />
     
     <button @click="addRecipe(newRecipe)">Ny Oppskrift</button>
     <p>Oppskriftstittler i en liste: {{ recipes }}</p>
