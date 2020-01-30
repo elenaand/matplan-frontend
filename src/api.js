570033e19e5cecfcf3986e385cf179cf3b8184f5
Eirik Rognø <eirik.rogno@gmail.com>
2020-01-30 20:59:25 +0100

    Starte på å implementere post til recipe

--- src/api.js
+++ src/api.js
@@ -11,3 +11,55 @@ export const getRecipes = () => {
       .then(res => resolve(res.json()));
   });
 };
+
+export const getIngredients = () => {
+  return new Promise(resolve => {
+    window
+      .fetch(BASE_URL + "/ingredient", {
+        headers: {
+          Accept: "application/json"
+        }
+      })
+      .then(res => resolve(res.json()));
+  });
+};
+
+// Example body for postRecipe
+/*{
+  "description": "Test rett nummer 1",
+  "tags": [],
+  "newIngredients": [
+		{
+			"name": "pasta",
+			"category": [MEAT, FROZEN, VEGETABLES, OTHER],
+			"amount": 1
+		}
+	],
+	"existingIngredients": [
+		{
+			"id": "1",
+			"amount": 2
+		}
+	]
+}*/
+export const postRecipe = (description, tags, newIngredients, existingIngredients) => {
+
+  const data = {
+    description: description,
+    tags: tags,
+    newIngredients: newIngredients,
+    existingIngredients: existingIngredients,
+  }
+
+  return new Promise(() => {
+    window
+      .fetch(BASE_URL + "/recipe", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+  });
+
+};
--- src/components/NewRecipe.vue
+++ src/components/NewRecipe.vue
@@ -3,40 +3,106 @@
     <label>Legg til ny oppskrift:</label>
     <br />
     
-    <textarea v-model="title" placeholder="Tittel" />
+    <textarea v-model="newRecipe.title" placeholder="Tittel" />
     <br />
-    <textarea v-model="tags" placeholder="Tags (liste av strenger)" />
+    <textarea v-model="newRecipe.tags" placeholder="Tags (liste av strenger)" />
     <br />
-    <select>
-      <option>En ingrediens</option>
-      <option>To ingrediens</option>
-      <option>Tre ingrediens</option>
+
+    <select v-model="selectedExistingIngredientId">
+      <option value="placeholder" disabled selected>Velg ingrediens</option>
+      <option
+        v-for="option in ingredient"
+        v-bind:value="option.id"
+        v-bind:key="option.id"
+        >{{ option.name }}</option
+      >
     </select>
     <br />
-    <textarea placeholder="ny ingrediens (navn, kategori, mengde)" />
+    <textarea v-model="newIngredient" placeholder="ny ingrediens (navn, kategori, mengde)" />
     
-    <button @click="addRecipe(title, tags)">Ny Oppskrift</button>
+    <button @click="addRecipe()">Ny Oppskrift</button>
     <p>Oppskriftstittler i en liste: {{ recipes }}</p>
   </div>
 </template>
 
 <script>
+import { postRecipe, getIngredients } from '../api.js';
+
 export default {
   name: "NewRecipe",
   data: function() {
-    return { newRecipe: {title: "", tags: ""}, recipes: [] };
+    return { 
+      newRecipe: {
+        title: "", 
+        tags: "",
+      }, 
+      newIngredient: "",
+      recipes: [],
+      ingredient: [],
+      selectedExistingIngredientId: "placeholder",
+    };
   },
   methods: {
-    addRecipe: function(title, tags) {
+    addRecipe: async function() {
+      const {title, tags} = this.newRecipe;
+
       if (title != "") {
         this.recipes.push(title);
       }
       if (tags != "") {
         this.recipes.push(tags);
       }
-      this.title = "";
-      this.tags = "";
-    }
+
+      const description = title;
+      const existingIngredients = [
+        {id: this.selectedExistingIngredientId, amount: 1} 
+      ];
+      const newIngredients = [
+        {
+          name: this.newIngredient.split(",")[0],
+          category: this.newIngredient.split(",")[1].trim(),
+          amount: parseInt(this.newIngredient.split(",")[2]),
+        }
+      ]
+
+      await postRecipe(description, tags.split(","), newIngredients, existingIngredients)
+
+      this.newRecipe = {
+        title: "",
+        tags: "",
+      };
+    },
+    /*return { 
+      recipes: [],
+
+      newIngredients: [],
+      existingIngredients: [],
+
+      newRecipeDescription: "", 
+      newIngredientName: "",
+      newIngredientAmount: "",
+      selectedExistingIngredientIndex: undefined,
+      existingIngredientAmount: "",
+    };
+  },
+  methods: {
+    addRecipe: function() {
+      this.recipes.push(newRecipeDescription);
+      this.newRecipe = "";
+    },
+    addNewIngredient: function() {
+
+      const newIngredient = {
+          
+      }
+
+    },
+    addExistingIngredient: function() {
+
+    },*/
+  },
+  mounted: async function() {
+    this.ingredient = await getIngredients();
   }
 };
 </script>
